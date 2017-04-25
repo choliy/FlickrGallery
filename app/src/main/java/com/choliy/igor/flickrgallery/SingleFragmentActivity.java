3712f9e354f4af553c60368822896b2827ef6fa7
Igor Choliy <choliy.igor@gmail.com>
2017-04-26 01:35:31 +0300

    Toolbar added

--- app/src/main/java/com/choliy/igor/flickrgallery/SingleFragmentActivity.java
+++ app/src/main/java/com/choliy/igor/flickrgallery/SingleFragmentActivity.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    private static final String TAG = SingleFragmentActivity.class.getSimpleName();
 
     protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_gallery);
 
         // First find out if some fragment is already in container,
@@ -26,5 +28,10 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+
+        // Setup custom toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
